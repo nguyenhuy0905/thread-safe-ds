b267972d40779fbf8e0fce0d54be12a998d16580
nguyenhuy0905 <nguyenhuy0905@users.noreply.github.com>
2025-02-23 19:55:49 +0000

    deploy: cf0d083b8ce74aba0b341fb69604657c50a6b5db

--- search/all_6.js
+++ search/all_6.js
@@ -3,5 +3,5 @@ var searchData=
   ['page_0',['Reference page',['../library_reference.html',1,'index']]],
   ['pool_20allocator_1',['Pool allocator',['../library_reference.html#pool_alloc',1,'']]],
   ['poolalloc_2',['PoolAlloc',['../classtsds_1_1PoolAlloc.html',1,'tsds']]],
-  ['poolalloc_3c_20other_2c_20nblock_20_3e_3',['PoolAlloc&lt; Other, NBlock &gt;',['../classtsds_1_1PoolAlloc.html',1,'tsds']]]
+  ['poolalloc_3c_20other_2c_20nblock_2c_20buffinitalloc_20_3e_3',['PoolAlloc&lt; Other, NBlock, BuffInitAlloc &gt;',['../classtsds_1_1PoolAlloc.html',1,'tsds']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['poolalloc_0',['PoolAlloc',['../classtsds_1_1PoolAlloc.html',1,'tsds']]],
-  ['poolalloc_3c_20other_2c_20nblock_20_3e_1',['PoolAlloc&lt; Other, NBlock &gt;',['../classtsds_1_1PoolAlloc.html',1,'tsds']]]
+  ['poolalloc_3c_20other_2c_20nblock_2c_20buffinitalloc_20_3e_1',['PoolAlloc&lt; Other, NBlock, BuffInitAlloc &gt;',['../classtsds_1_1PoolAlloc.html',1,'tsds']]]
 ];
