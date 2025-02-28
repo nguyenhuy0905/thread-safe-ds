f54875a1d5639420f1ea132df692524879740c28
nguyenhuy0905 <nguyenhuy0905@users.noreply.github.com>
2025-02-28 16:26:37 +0000

    deploy: 8ef13f9055a7d282422d9de6697e6fe3482560dd

--- classtsds_1_1ArenaAlloc_1_1AllocBuff.js
+++ classtsds_1_1ArenaAlloc_1_1AllocBuff.js
@@ -1,5 +1,5 @@
 var classtsds_1_1ArenaAlloc_1_1AllocBuff =
 [
-    [ "allocate", "classtsds_1_1ArenaAlloc.html#ae86a643e8eb4e54a9ad33be7354f13f6", null ],
+    [ "allocate", "classtsds_1_1ArenaAlloc.html#ab757bd6bc9a1ada9d19b957973939dc7", null ],
     [ "deallocate", "classtsds_1_1ArenaAlloc.html#aebf63fd1d47e56e5a30df6426ab7337e", null ]
 ];
--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['allocate_0',['allocate',['../classtsds_1_1ArenaAlloc.html#ae86a643e8eb4e54a9ad33be7354f13f6',1,'tsds::ArenaAlloc::allocate()'],['../classtsds_1_1PoolAlloc.html#a93c314a5fc5117cf2b76aee6d7c65cef',1,'tsds::PoolAlloc::allocate()'],['../classtsds_1_1ArenaAlloc.html#ae86a643e8eb4e54a9ad33be7354f13f6',1,'tsds::ArenaAlloc::AllocBuff::allocate()']]],
+  ['allocate_0',['allocate',['../classtsds_1_1ArenaAlloc.html#ab757bd6bc9a1ada9d19b957973939dc7',1,'tsds::ArenaAlloc::allocate()'],['../classtsds_1_1PoolAlloc.html#a93c314a5fc5117cf2b76aee6d7c65cef',1,'tsds::PoolAlloc::allocate()'],['../classtsds_1_1ArenaAlloc.html#ab757bd6bc9a1ada9d19b957973939dc7',1,'tsds::ArenaAlloc::AllocBuff::allocate()']]],
   ['allocator_1',['allocator',['../library_reference.html#arena_alloc',1,'Arena allocator'],['../library_reference.html#pool_alloc',1,'Pool allocator']]],
   ['allocators_2',['Allocators',['../library_reference.html#alloc',1,'']]],
   ['allocbuff_3',['AllocBuff',['../classtsds_1_1ArenaAlloc_1_1AllocBuff.html',1,'tsds::ArenaAlloc']]],
